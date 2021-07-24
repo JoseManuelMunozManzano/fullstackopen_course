bcc435dd7b04d2d9298379b3c5dd9423a34899b5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-07-24 18:35:13 +0200

    Exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = () => <h1>give feedback</h1>;
@@ -9,9 +9,9 @@ const Button = ({ text, handleClick }) => {
 
 const StatisticsHeader = () => <h1>statistics</h1>;
 
-const StatisticsText = ({ text, value }) => (
+const StatisticsText = ({ text, value, sign = '' }) => (
   <p>
-    {text} {value}
+    {text} {value} {sign}
   </p>
 );
 
@@ -20,19 +20,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = useRef(0);
+  const average = useRef(0);
+  const positive = useRef(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  all.current = good + neutral + bad;
+  if (all.current) {
+    average.current = (good - bad) / (good + neutral + bad);
+    positive.current = (good * 100) / (good + neutral + bad);
+  }
+
   return (
     <div>
       <Header />
 
-      <Button text="good" handleClick={() => setGood(good + 1)} />
-      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
 
       <StatisticsHeader />
 
       <StatisticsText text="good" value={good} />
       <StatisticsText text="neutral" value={neutral} />
       <StatisticsText text="bad" value={bad} />
+      <StatisticsText text="all" value={all.current} />
+      <StatisticsText text="average" value={average.current} />
+      <StatisticsText text="positive" value={positive.current} sign="%" />
     </div>
   );
 };
