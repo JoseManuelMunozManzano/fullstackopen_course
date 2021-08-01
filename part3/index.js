c3af327b24e795bb4fef6dbe752d84666b02c286
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-01 07:22:41 +0200

    Exercise 3.8

--- part3/index.js
+++ part3/index.js
@@ -30,12 +30,28 @@ let persons = [
   },
 ];
 
+// Functions
+const logger = (tokens, req, res) => {
+  morgan.token('body', function (req, res) {
+    return JSON.stringify(req.body);
+  });
+
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    tokens['response-time'](req, res),
+    'ms',
+    tokens.body(req, res),
+  ].join(' ');
+};
+
 // Middlewares
 app.use(express.json());
 
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms')
-);
+app.use(morgan(logger));
 
 // Routes
 app.get('/info', (req, res) => {
