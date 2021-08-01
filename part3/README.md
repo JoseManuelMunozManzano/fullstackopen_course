7aa9f2ca9cd4e6eacb76f2c2c555fbad906d0924
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-01 17:44:22 +0200

    Exercise 3.10

--- part3/README.md
+++ part3/README.md
@@ -1,3 +1,3 @@
 # Exercises 3.1 - 3.6, 3.7 - 3.8, 3.9 - 3.11
 
-Heroku url:
+Heroku url (backend): https://sheltered-garden-81718.herokuapp.com/api/persons
--- part3/index.js
+++ part3/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 // Consts and variables
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const MAX_ID = 1_000_000;
 
 let persons = [
