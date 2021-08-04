0e59775294ba6817a786db90cddc46668b0e3f00
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-04 18:24:23 +0200

    Exercises 3.13 and 3.14

--- part3/README.md
+++ part3/README.md
@@ -1,4 +1,4 @@
-# Exercises 3.1 - 3.6, 3.7 - 3.8, 3.9 - 3.11, 3.12
+# Exercises 3.1 - 3.6, 3.7 - 3.8, 3.9 - 3.11, 3.12, 3.13 - 3.14
 
 Heroku url (only backend data): https://sheltered-garden-81718.herokuapp.com/api/persons
 
--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 
@@ -8,28 +10,28 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const MAX_ID = 1_000_000;
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-];
+// let persons = [
+//   {
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//     id: 1,
+//   },
+//   {
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//     id: 2,
+//   },
+//   {
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//     id: 3,
+//   },
+//   {
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//     id: 4,
+//   },
+// ];
 
 // Functions
 const logger = (tokens, req, res) => {
@@ -57,31 +59,50 @@ app.use(cors());
 
 // Routes
 app.get('/info', (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
-  );
+  Person.find({}).then((persons) => {
+    res.send(
+      `<p>Phonebook has info for ${
+        persons.length
+      } people</p><p>${new Date()}</p>`
+    );
+  });
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.findById(id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
-  persons = persons.filter((person) => person.id !== id);
+  const id = req.params.id;
 
-  res.status(204).end();
+  Person.deleteOne({ _id: id })
+    .then((result) => {
+      if (result.deletedCount > 0) {
+        res.status(204).end();
+      } else {
+        res.status(400).json({ error: 'person was deleted before' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 app.post('/api/persons', (req, res) => {
@@ -95,24 +116,14 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'number is required' });
   }
 
-  if (
-    persons.find(
-      (person) =>
-        person.name.toUpperCase().trim() === body.name.toUpperCase().trim()
-    )
-  ) {
-    return res.status(404).json({ error: 'name must be unique' });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name.trim(),
     number: body.number.trim(),
-    id: Math.floor(Math.random() * MAX_ID) + 1,
-  };
+  });
 
-  persons = [...persons, person];
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 // Port
