71b22a930bb50791eb188d07575c140730796965
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-08 06:43:58 +0200

    Exercise 4.2 second refactoring

--- part4/app.js
+++ part4/app.js
@@ -1,17 +1,9 @@
+const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
 const mongoose = require('mongoose');
-const config = require('./utils/config');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 mongoose.connect(config.mongoUrl, {
   useNewUrlParser: true,
@@ -22,19 +14,6 @@ mongoose.connect(config.mongoUrl, {
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
