4ef345cf7d10a3c6a215ec9238d3517d74c0ca02
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-08 09:12:47 +0200

    Exercise 4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -10,7 +12,25 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const { author } = _.maxBy(blogs, (blog) => blog.author);
+  const numberBlogs = _.reduce(
+    blogs,
+    (sum, blog) => {
+      return blog.author === author ? sum + 1 : sum;
+    },
+    0
+  );
+
+  return { author, blogs: numberBlogs };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 };
