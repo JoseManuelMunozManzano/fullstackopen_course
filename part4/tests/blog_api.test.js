005cde4fede45279b81305298fc79bda50d9ee71
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-09 18:49:43 +0200

    Exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const helper = require('./data');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog');
+
+describe('testing blog api', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    const blogObjects = helper.blogs.map((blog) => new Blog(blog));
+    const promiseArray = blogObjects.map((blog) => blog.save());
+    await Promise.all(promiseArray);
+  });
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(helper.blogs.length);
+  });
+
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+});
