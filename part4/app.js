2c07cd125706b9a0705aada46018d1e20a88a93b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-14 20:25:47 +0200

    Exercise 4.15

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const mongoose = require('mongoose');
 
 mongoose.connect(config.mongoUrl, {
@@ -16,5 +17,6 @@ mongoose.connect(config.mongoUrl, {
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
