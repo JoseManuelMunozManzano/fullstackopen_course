7576cf5784d3b90dcdd7108caf1ffa003adfa7b0
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-19 20:26:10 +0200

    Exercise 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,5 +1,35 @@
-export const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+export const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={handleDetails}>{showDetails ? 'hide' : 'view'}</button>
+        {showDetails && (
+          <div>
+            <div>{blog.url}</div>
+            <div>
+              likes {blog.likes} <button>like</button>
+            </div>
+            <div>{blog.user.name}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
