c111eae360cf42971348dcf19992f4902778e2b8
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-20 20:25:14 +0200

    Exercise 5.9

--- part5/src/App.js
+++ part5/src/App.js
@@ -132,9 +132,11 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} addLike={addLike} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} addLike={addLike} />
+        ))}
     </div>
   );
 
