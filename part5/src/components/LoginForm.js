68532c530fce5c7ffcf78c9e01d34489424ea732
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-08-21 05:28:27 +0200

    Exercise 5.11

--- part5/src/components/LoginForm.js
+++ part5/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const LoginForm = ({
   handleSubmit,
@@ -34,4 +35,12 @@ export const LoginForm = ({
       </form>
     </div>
   );
+};
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
